e0aae6a08b008b7579c3d7d68dca2357846e93d7
Vĩnh Kỳyy <vinhkyle123@gmail.com>
2024-05-29 14:08:04 +0700

    nothing

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_file
 from flask_cors import CORS
 from process import crawl as cr
 from process import preprocessing as pp
 from model import modeling as md
+from matplotlib import pyplot as plt
+import io
 
 import os
 import warnings
@@ -26,8 +28,7 @@ def process():
             result_pos, result_neg, top_comment_positive, top_comment_negative, top_words_positive, top_words_negative = md.modeling(data)
         else:
             result_neg, result_pos, top_comment_positive, top_comment_negative, top_words_positive, top_words_negative = 0, 0, [], [], [], []
-        
-        print(result_pos, result_neg, top_comment_positive, top_comment_negative, top_words_positive, top_words_negative)
+
         return jsonify({"positive": result_pos, 
                         "negative": result_neg, 
                         "top_positive_comments": top_comment_positive, 
@@ -38,6 +39,26 @@ def process():
     except Exception as e:
         print(e)
         return jsonify({"error": str(e)}), 500
+'''
+@app.route('/drawPLot', methods=['POST'])
+def draw_plot():
+    # Get the data from the request (you'll need to parse the JSON)
+    # For simplicity, I'm using fixed data
+    data = {'red': 10, 'green': 20, 'grey': 70}
+    labels = list(data.keys())
+    sizes = list(data.values())
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+    ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    bytes_image = io.BytesIO()
+    plt.savefig(bytes_image, format='png')
+    bytes_image.seek(0)
+
+    return send_file(bytes_image, mimetype='image/png')
+
+'''
 
 if __name__ == '__main__':
     app.run(debug=True)
